213d26d2b97b822f71d123c25814e8cea125f975
Ragul <ragulkumarv333@gmail.com>
2024-01-28 17:05:01 +0530

    Update RestaurentCard.js

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -2,21 +2,14 @@ import { ASSET_PATH } from "../utils/staticData";
 
 const RestaurentCard = (props) => {
   const { respData } = props;
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    deliveryTime,
-    cuisines,
-    locality,
-  } = respData?.info;
+  const { cloudinaryImageId, name, avgRating, sla, cuisines, locality } =
+    respData?.info;
   return (
     <section className="card-container">
       <img src={ASSET_PATH + cloudinaryImageId} alt="res-logo" />
       <h1 className="res-name">{name}</h1>
       <div className="ratings-eta">
-        <span>{avgRating}</span>
-        <span>{deliveryTime}</span>
+        <span>{avgRating}</span>&nbsp; • &nbsp;<span>{sla?.slaString}</span>
       </div>
       <div className="cuisine">{cuisines.join(", ")}</div>
       <div className="location">{locality}</div>
