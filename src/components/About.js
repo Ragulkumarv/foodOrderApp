ff69d7589c389a0050c510fa2cb5f8c8ace4f004
Ragul <ragulkumarv333@gmail.com>
2024-02-10 19:50:54 +0530

    class component added episode-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <h3>
       Our mission is to elevate the quality of life for the urban consumer with
       unparalleled convenience. Convenience is what makes us tick. It's what
       makes us get out of bed and say, "Let's do this."
+      <UserClass stream={"Residing @"} location={"Coimbatore"} />
     </h3>
   );
 };
---
+++ src/components/UserClass.js
@@ -0,0 +1,46 @@
+import React from "react";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        login: "User",
+        public_repos: "Coimbatore",
+      },
+    };
+    console.log("constructor");
+  }
+
+  async componentDidMount() {
+    const url = await fetch("https://api.github.com/users/Ragulkumarv");
+    const resp = await url.json();
+    console.log(resp);
+    this.setState({
+      userInfo: resp,
+    });
+
+    console.log("component did mount");
+  }
+
+  componentWillUnmount() {
+    console.log("component did unmount");
+  }
+
+  render() {
+    console.log("return func");
+    const { login, public_repos } = this.state.userInfo;
+    const { stream, location } = this.props;
+
+    return (
+      <div>
+        <h1>Hi !!! {login}</h1>
+        <h2>Public Repos {public_repos} </h2>
+        <h3>
+          {stream} - {location}
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
